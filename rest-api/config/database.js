9143e19c2094f31f9b2513c64bf560c01ec79005
Alexander Farkov <alex58020@gmail.com>
2021-03-29 15:13:30 +0300

    env vars changes

--- rest-api/config/database.js
+++ rest-api/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
-const { DB_URI } = require('./config');
+// const { DB_URI } = require('./config');
 
-mongoose.connect(DB_URI, {
+mongoose.connect(process.env.DB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
